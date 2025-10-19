242e484a5dc9d2c7947ae33da2efba80dbc9d5f1
Alan Smith <commit@alanwsmith.com>
2025-10-19 11:35:59 -0400

    added description

--- content/_includes/script.js
+++ content/_includes/script.js
@@ -1,3 +1,7 @@
+const items = [
+  "eyepatch", "glasses", "glasses-2", "glasses-3", "glasses-4", "glasses-5", "sunglasses", "sunglasses-2",
+];
+
 const t = {
   face: `
     <div class="face-item" data-receive="updateFaceItem" data-item="ITEM"></div>`, 
@@ -9,9 +13,6 @@ const t = {
       data-receive="updateShelfItem" 
       data-item="ITEM"></button>`,
 }
-const items = [
-  "eyepatch", "glasses", "glasses-2", "glasses-3", "glasses-4", "glasses-5", "sunglasses", "sunglasses-2",
-];
 
 window.Trader = class {
   async body(_event, el){
@@ -35,7 +36,6 @@ window.Trader = class {
     for (let key of items) {
       let subs = [["ITEM", key]];
       let svg = await this.api.fetchSVG(`/svgs/accessories/${key}.svg`);
-      svg.classList.add("face-svg");
       let item = await this.api.useEl(t.shelf, subs);
       item.appendChild(svg);
       el.appendChild(item);
@@ -58,7 +58,4 @@ window.Trader = class {
     }
     this.api.forward(event, "updateFaceItem");
   }
-
-
-
 };
