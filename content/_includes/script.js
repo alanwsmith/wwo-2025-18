a4dbb6718b68ce86f2414fcaf96a8450720170e9
Alan Smith <commit@alanwsmith.com>
2025-10-19 11:15:13 -0400

    switched accessories in place

--- content/_includes/script.js
+++ content/_includes/script.js
@@ -1,22 +1,19 @@
 const t = {
   face: `
-    <div data-receive="hideFaceItem" data-item="ITEM"></div>`, 
-shelf: `
-  <button 
-    class="shelf-item"
-    data-send="updateItem" 
-    data-receive="updateItem" 
-    data-item="ITEM"></button>`,
+    <div class="face-item" data-receive="updateFaceItem" data-item="ITEM"></div>`, 
+
+  shelf: `
+    <button 
+      class="shelf-item"
+      data-send="updateShelfItem" 
+      data-receive="updateShelfItem" 
+      data-item="ITEM"></button>`,
 }
 const items = [
   "eyepatch", "glasses", "glasses-2", "glasses-3", "glasses-4", "glasses-5", "sunglasses", "sunglasses-2",
 ];
 
 window.Trader = class {
-  #current = null;
-
-  bittyInit() {}
-
   async body(_event, el){
     let svg = await this.api.fetchSVG("/svgs/body/thunder-t-shirt.svg");
     el.appendChild(svg);
@@ -28,18 +25,10 @@ window.Trader = class {
       let subs = [["ITEM", key]];
       let svg = await this.api.fetchSVG(`/svgs/accessories/${key}.svg`);
       let item = await this.api.useEl(t.face, subs);
-      // item.hidden = true;
+      item.classList.add("hidden");
       item.appendChild(svg);
       el.appendChild(item);
     }
-  }
-
-
-  faceItem(event, el) {
-  }
-
-  switchFaceItem(event, el) {
-
   }
 
   async shelf(_event, el) {
@@ -53,13 +42,23 @@ window.Trader = class {
     }
   }
 
-  updateItem(event, el) {
+  updateFaceItem(event, el) {
+    if (this.api.match(event, el, "item")) {
+      el.classList.remove("hidden");
+    } else {
+      el.classList.add("hidden");
+    }
+  }
+
+  updateShelfItem(event, el) {
     if (this.api.match(event, el, "item")) {
       el.classList.add("current");
-      console.log(el);
     } else {
       el.classList.remove("current");
     }
+    this.api.forward(event, "updateFaceItem");
   }
+
+
 
 };
